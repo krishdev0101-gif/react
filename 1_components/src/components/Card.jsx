75bb3bfd94b466f84e3bce2b58f392d527f86fd1
Krish <krish.dev0101@gmail.com>
2026-05-19 13:34:52 +0000

    Components

---
+++ 1_components/src/components/Card.jsx
@@ -0,0 +1,8 @@
+function Card() {
+    return <div className="card">
+        <h1>Krish</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, voluptate.</p>
+    </div>
+}
+
+export default Card;
