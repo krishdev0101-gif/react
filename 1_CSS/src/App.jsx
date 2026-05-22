6e5dfd975f6553ab5f053d6933aed8d70d3d07e8
Krish <krish.dev0101@gmail.com>
2026-05-22 12:17:49 +0000

    CSS

---
+++ 1_CSS/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Header from './components/header/Header'
+import Button from './components/button/Button'
+
+const App = () => {
+  return (
+    <div>
+      <Button />
+      <Header />
+    </div>
+  )
+}
+
+export default App
---
+++ 1_CSS/src/components/button/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './Button.module.css'
+
+const Button = () => {
+  return (
+    <div className={styles.btn}>
+      This Is button
+    </div>
+  )
+}
+
+export default Button
